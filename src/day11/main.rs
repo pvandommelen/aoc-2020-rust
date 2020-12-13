837c943c5c35c1a7ebe3e2deebf190f2650f0452
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2020-12-13 17:25:40 +0100

    Day 11 part 2, removed fancy windowing solution.

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -9,18 +9,23 @@ pub struct Map<T: Copy> {
     stride_y: usize,
 }
 
-type Window<T> = Vec<T>;
-
 trait QueryableMap<T> {
-    fn at(&self, x: usize, y: usize) -> T;
+    fn at(&self, x: i32, y: i32) -> Option<T>;
+    fn at_unchecked(&self, x: usize, y: usize) -> T;
+
     fn set(&mut self, x: usize, y: usize, value: &T);
 
     fn iter(&'_ self) -> Box<dyn Iterator<Item = (usize, usize, T)> + '_>;
-
-    fn iter_window(&'_ self, distance: usize, default_value: T) -> Box<dyn Iterator<Item = (usize, usize, Window<T>)> + '_>;
 }
 impl<T: Copy> QueryableMap<T> for Map<T> {
-    fn at(&self, x: usize, y: usize) -> T {
+    fn at(&self, x: i32, y: i32) -> Option<T> {
+        if x < 0 || x >= self.width as i32 || y < 0 || y >= self.height as i32 {
+            None
+        } else {
+            Some(self.at_unchecked(x as usize, y as usize))
+        }
+    }
+    fn at_unchecked(&self, x: usize, y: usize) -> T {
         let index = y * self.stride_y + x;
         self.data[index]
     }
@@ -36,32 +41,7 @@ impl<T: Copy> QueryableMap<T> for Map<T> {
                     (x, y)
                 })
             }).map(move |(x, y)| {
-                (x, y, self.at(x, y))
-            })
-        )
-    }
-
-    fn iter_window(&'_ self, distance: usize, default_value: T) -> Box<dyn Iterator<Item = (usize, usize, Window<T>)> + '_> {
-        let window_side_length = distance * 2 + 1;
-        let window_size = window_side_length * window_side_length;
-
-        Box::new(
-            (0..self.height).flat_map(move |y| {
-                (0..self.width).map(move |x| {
-                    (x, y)
-                })
-            }).map(move |(x, y)| {
-                let mut window = vec![default_value; window_size];
-                for j in 0..window_side_length {
-                    for i in 0..window_side_length {
-                        window[j * window_side_length + i] = if i + x < distance || j + y < distance || i + x - distance >= self.width || j + y - distance >= self.height {
-                            default_value
-                        } else {
-                            self.at(i + x - distance, j + y - distance)
-                        };
-                    }
-                }
-                (x, y, window)
+                (x, y, self.at_unchecked(x, y))
             })
         )
     }
@@ -79,26 +59,36 @@ fn prepare_input(input: &str) -> Map<u8> {
     }
 }
 
-fn evolve(map: &Map<u8>, state: &Map<bool>) -> (Map<bool>, bool) {
+fn evolve1(map: &Map<u8>, state: &Map<bool>) -> (Map<bool>, bool) {
     let mut new_state = state.to_owned();
     let mut changed = false;
-    state.iter_window(1, false).for_each(
-        |(x, y, old_state_window)| {
+    map.iter().for_each(
+        |(x, y, elem)| {
+            if elem != b'L' {
+                return;
+            }
 
-            if map.at(x, y) == b'L' {
-                let old_seat_state = state.at(x, y);
-                let filled_seats_in_window_count = old_state_window.iter().filter(|&val| *val).count();
-
-                let filled_seats_adjacent = if old_seat_state { filled_seats_in_window_count - 1 } else { filled_seats_in_window_count };
-
-                let new_seat_state = match old_seat_state {
-                    false => filled_seats_adjacent == 0,
-                    true => filled_seats_adjacent < 4,
-                };
-                if new_seat_state != old_seat_state {
-                    new_state.set(x, y, &new_seat_state);
-                    changed = true;
+            let mut seats_filled: u8 = 0;
+            for j in (-1 as i32)..2 {
+                for i in (-1 as i32)..2 {
+                    let position_x = x as i32 + i;
+                    let position_y = y as i32 + j;
+                    if let Some(square) = map.at(position_x, position_y) {
+                        if square == b'L' {
+                            seats_filled += if state.at(position_x, position_y).unwrap() { 1 } else { 0 };
+                        }
+                    }
                 }
+            }
+
+            let old_seat_state = state.at_unchecked(x, y);
+            let new_seat_state = match old_seat_state {
+                false => seats_filled == 0,
+                true => seats_filled <= 4,
+            };
+            if new_seat_state != old_seat_state {
+                new_state.set(x, y, &new_seat_state);
+                changed = true;
             }
         }
     );
@@ -115,7 +105,7 @@ fn part1(input: &Map<u8>) -> usize {
     };
 
     loop {
-        let (evolved_state, changed) = evolve(input, &state);
+        let (evolved_state, changed) = evolve1(input, &state);
         if !changed {
             break;
         }
@@ -124,8 +114,64 @@ fn part1(input: &Map<u8>) -> usize {
 
     state.iter().filter(|(_, _, value)| *value).count()
 }
-fn part2(input: &Map<u8>) -> u32 {
-    0
+
+fn evolve2(map: &Map<u8>, state: &Map<bool>) -> (Map<bool>, bool) {
+    let mut new_state = state.to_owned();
+    let mut changed = false;
+    map.iter().for_each(
+        |(x, y, elem)| {
+            if elem != b'L' {
+                return;
+            }
+
+            let mut seats_filled: u8 = 0;
+            for j in (-1 as i32)..2 {
+                for i in (-1 as i32)..2 {
+                    let mut position_x = x as i32 + i;
+                    let mut position_y = y as i32 + j;
+                    while let Some(square) = map.at(position_x, position_y) {
+                        if square == b'L' {
+                            seats_filled += if state.at(position_x, position_y).unwrap() { 1 } else { 0 };
+                            break;
+                        }
+                        position_x += i;
+                        position_y += j;
+                    }
+                }
+            }
+
+            let old_seat_state = state.at_unchecked(x, y);
+            let new_seat_state = match old_seat_state {
+                false => seats_filled == 0,
+                true => seats_filled <= 5,
+            };
+            if new_seat_state != old_seat_state {
+                new_state.set(x, y, &new_seat_state);
+                changed = true;
+            }
+        }
+    );
+    (new_state, changed)
+}
+
+fn part2(input: &Map<u8>) -> usize {
+    let data = vec![false; input.width * input.height];
+    let mut state = Map {
+        data,
+        width: input.width,
+        height: input.height,
+        stride_y: input.width,
+    };
+
+    loop {
+        let (evolved_state, changed) = evolve2(input, &state);
+        if !changed {
+            break;
+        }
+        state = evolved_state;
+    }
+
+    state.iter().filter(|(_, _, value)| *value).count()
 }
 
 fn main() {
@@ -160,17 +206,16 @@ L.LLLLL.LL";
         let map = prepare_input(EXAMPLE_INPUT);
         assert_eq!(map.width, 10);
         assert_eq!(map.height, 10);
-        assert_eq!(map.at(0, 0), b'L');
-        assert_eq!(map.at(9, 0), b'L');
-        assert_eq!(map.at(0, 1), b'L');
-        assert_eq!(map.at(9, 9), b'L');
+        assert_eq!(map.at(0, 0), Some(b'L'));
+        assert_eq!(map.at(9, 0), Some(b'L'));
+        assert_eq!(map.at(0, 1), Some(b'L'));
+        assert_eq!(map.at(9, 9), Some(b'L'));
     }
     
     #[test]
-    fn map_iter_window() {
+    fn map_iter() {
         let map = prepare_input(EXAMPLE_INPUT);
-        assert_eq!(map.iter_window(0, 0).count(), 10*10);
-        assert_eq!(map.iter_window(1, 0).count(), 10*10);
+        assert_eq!(map.iter().count(), 10*10);
     }
     
     #[test]
@@ -180,6 +225,6 @@ L.LLLLL.LL";
     
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&prepare_input(EXAMPLE_INPUT)), 0);
+        assert_eq!(part2(&prepare_input(EXAMPLE_INPUT)), 26);
     }
 }
